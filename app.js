86115c51e134dbb4d090614a28260ba6a24662bc
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-08-28 16:58:50 +0200

    cannot access post param probably due to lack of some middleware

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views') )
+app.set('views', path.join(__dirname, 'views') ) //function from path module
 
-app.get('/', function(request, response){
-  response.render('index')
+app.get('/', function(request, response){ // second arg is an object containing all of the data needed in a view
+  response.render('index', { title: 'Sample applciation - node.js', items: [{desc: 'foo'}, {title: 'bar'}] }) // uses path and extesion(ejs) from cofiguration
+})
+
+app.post('/new', function(request, response){ // second arg is an object containing all of the data needed in a view
+  var item = request.body.newItem
+  console.log(item)
 })
 
 app.listen(1337, function(){
