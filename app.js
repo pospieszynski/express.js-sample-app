be8b6897779140de9cdf845953927ec411b72e68
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-08-28 16:19:36 +0200

    added conf to enable views rendering

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views') )
+
 app.get('/', function(request, response){
-  response.render('./views/index.ejs')
+  response.render('index')
 })
 
 app.listen(1337, function(){
