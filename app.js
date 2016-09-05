11e97c28b4b7eb04d61f227aaf0a3ae4a937f072
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-09-05 03:43:43 +0200

    archived

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser());
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views') ) //function from path module
@@ -13,6 +16,7 @@ app.get('/', function(request, response){ // second arg is an object containing 
 app.post('/new', function(request, response){ // second arg is an object containing all of the data needed in a view
   var item = request.body.newItem
   console.log(item)
+  response.redirect('/')
 })
 
 app.listen(1337, function(){
